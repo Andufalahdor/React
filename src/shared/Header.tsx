47236eaadeca11432440e27c7e3a941e2ee4c0b7
Nikita Evstratov <nikit.evstratov@gmail.com>
2022-05-13 19:39:01 +0300

    add StarWarsNameGenerator

--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -1,13 +1,48 @@
 import { hot } from 'react-hot-loader/root';
-import * as React from 'react';
+
+import React from 'react';
 import styles from './header.less'
+import { starWars, uniqueNamesGenerator } from 'unique-names-generator';
+
+interface IStarWarsNameClassState {
+    name: string;
+    count: number;
+}
 
 function HeaderComponent() {
     return (
-        <header>
-            <h1 className={styles.example}>Hello React!!!</h1> 
-        </header>
+            <StarWarsNameClass></StarWarsNameClass>
     )
+}
+
+class StarWarsNameClass extends React.PureComponent<{}, IStarWarsNameClassState> {
+
+    // Состояние через перегрузку
+    state: Readonly<IStarWarsNameClassState> = { name: this.randomName(), count: 0 } // READONLY! !!
+
+    // Состояния через конструктор 
+    // constructor(props: {}) {
+    //     super(props)
+    //     this.state = { name: this.randomName() }
+    // }
+
+    render() {
+        return (
+        <section>
+            <div className={styles.example}>{this.state.name}</div>
+            <button onClick={this.handleClick}> Give me name!</button>
+        </section>
+        );
+    }
+
+    private handleClick = () => {
+        this.setState( {name: this.randomName() } )
+        this.setState( (state, props) => ({count: state.count + 1}), () => {console.log(this.state.count)})
+    };
+
+    private randomName(): string {
+        return uniqueNamesGenerator({dictionaries: [starWars], length: 1})
+    }    
 }
 
 export const Header = hot(HeaderComponent);
