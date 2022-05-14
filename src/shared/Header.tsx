0f32e33aca1d80e522ae7f68f3aa1199d9250ec4
Nikita Evstratov <nikit.evstratov@gmail.com>
2022-05-14 12:02:52 +0300

    add functional components / example

--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -4,21 +4,30 @@ import React from 'react';
 import styles from './header.less'
 import { starWars, uniqueNamesGenerator } from 'unique-names-generator';
 
-interface IStarWarsNameClassState {
-    name: string;
-    count: number;
+// interface IStarWarsNameClassState {
+//     name: string;
+//     count: number;
+// }
+
+interface IStarWarsNameFunctionState {
+name: string;
+count: number;
 }
 
 function HeaderComponent() {
     return (
-            <StarWarsNameClass></StarWarsNameClass>
+            // <StarWarsNameClass></StarWarsNameClass> // Компонент class 
+            <StarWarsNameFunction></StarWarsNameFunction> // Компонент function
+
     )
 }
 
-class StarWarsNameClass extends React.PureComponent<{}, IStarWarsNameClassState> {
+// class Components
+
+// class StarWarsNameClass extends React.PureComponent<{}, IStarWarsNameClassState> {
 
     // Состояние через перегрузку
-    state: Readonly<IStarWarsNameClassState> = { name: this.randomName(), count: 0 } // READONLY! !!
+    // state: Readonly<IStarWarsNameClassState> = { name: this.randomName(), count: 0 } // READONLY! !!
 
     // Состояния через конструктор 
     // constructor(props: {}) {
@@ -26,23 +35,63 @@ class StarWarsNameClass extends React.PureComponent<{}, IStarWarsNameClassState>
     //     this.state = { name: this.randomName() }
     // }
 
-    render() {
-        return (
-        <section>
-            <div className={styles.example}>{this.state.name}</div>
-            <button onClick={this.handleClick}> Give me name!</button>
-        </section>
-        );
+//     render() {
+//         return (
+//         <section>
+//             <div className={styles.example}>{this.state.name}</div>
+//             <button onClick={this.handleClick}> Give me name!</button>
+//         </section>
+//         );
+//     }
+
+//     private handleClick = () => {
+//         this.setState( {name: this.randomName() } )
+//         this.setState( (state, props) => ({count: state.count + 1}), () => {console.log(this.state.count)})
+//     };
+
+//     private randomName(): string {
+//         return uniqueNamesGenerator({dictionaries: [starWars], length: 1})
+//     }    
+// }
+
+
+
+// Functional Components 
+
+export function StarWarsNameFunction() {
+    const randomName = () => uniqueNamesGenerator({dictionaries: [starWars], length: 1})
+
+    // // Объявляем состояние через хук. 
+    // const [name, setName] = React.useState(randomName);
+    // // Атомарное состояния
+    // const [count, setCount] = React.useState(0);
+
+    // const handleClick = () => {
+    //     setName(randomName);
+    //     setCount((prevCount: number) => prevCount + 1);
+    // };
+    // console.log(count);
+
+    // Объявляем Комплексное состояние в виде объекта 
+    const [state, setState] = React.useState<IStarWarsNameFunctionState>({name: randomName(), count: 0})
+    
+    const handleClick = () => {
+        // setState ( (prevState: IStarWarsNameFunctionState) => ({ name: randomName(), count: prevState.count + 1 }));
+
+        // изменения состояния по отдельности с помощью спред (...prevState)
+        setState ( (prevState: IStarWarsNameFunctionState) => ({...prevState, name: randomName() }));
+        setState ( (prevState: IStarWarsNameFunctionState) => ({...prevState, count: prevState.count + 1 }));
     }
 
-    private handleClick = () => {
-        this.setState( {name: this.randomName() } )
-        this.setState( (state, props) => ({count: state.count + 1}), () => {console.log(this.state.count)})
-    };
+    console.log(state.count);
 
-    private randomName(): string {
-        return uniqueNamesGenerator({dictionaries: [starWars], length: 1})
-    }    
+    return (
+        <section>
+            <div className={styles.example}> {state.name} </div>
+            <button onClick={handleClick}> Give me name!</button>
+        </section>
+    )
 }
 
+// HMR
 export const Header = hot(HeaderComponent);
