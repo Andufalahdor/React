3f1b6d6ed190b205b2a5510605e832c038cc8f77
Nikita Evstratov <nikit.evstratov@gmail.com>
2023-01-08 18:16:49 +0200

    add examaple jest ts file

---
+++ src/example.test.ts
@@ -0,0 +1,15 @@
+// test("Should return 2 when add one and one", () => {
+//   expect(2 + 0).toBe(2);
+// });
+
+describe("Add operator", () => {
+  test("Should return 2 when add one and one", () => {
+    expect(2 + 0).toBe(2);
+  });
+
+  //   describe("Again Add operator", () => {
+  //     test("Should return 2 when add one and one", () => {
+  //       expect(2 + 0).toBe(2);
+  //     });
+  //   });
+});
